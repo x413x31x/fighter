0a408e185494beb7c6f5559109a32da1cbdf8d7e
413x31 <x413x31x@gmail.com>
2023-03-24 17:30:49 +0200

    UI
    добавлено новий ХБ
    добавлено спрайти на джойстики
    
    24,03 (1 hours)

--- fighter/Assets/Scripts/Healthbar/HealthbarManager.cs
+++ fighter/Assets/Scripts/Healthbar/HealthbarManager.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class HealthbarManager : MonoBehaviour
 {
     private CharacterStateManager _character;
     private float _maxHealth;
     private Image _slider;
+    private TMP_Text _healthCounter;
+    [SerializeField] private Sprite _yellow;
+    [SerializeField] private Sprite _red;
     [SerializeField] private bool _isPlayer;
 
     private void Start()
@@ -20,10 +24,20 @@ public class HealthbarManager : MonoBehaviour
         }
         _maxHealth = _character._maxHealth;
         _slider = GetComponent<Image>();
+        _healthCounter = GetComponentInChildren<TMP_Text>();
     }
 
     private void Update()
     {
+        if(_character._currentHealth / _maxHealth <= 0.6 && _character._currentHealth / _maxHealth > 0.3)
+        {
+            _slider.sprite = _yellow;
+        }
+        else if(_character._currentHealth / _maxHealth <= 0.3)
+        {
+            _slider.sprite = _red;
+        }
+        _healthCounter.text = _character._currentHealth.ToString();
         _slider.fillAmount = _character._currentHealth / _maxHealth;
     }
 }
